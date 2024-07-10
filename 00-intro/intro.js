906cb7bd28fc200cb4597607e75c42f699523364
christian-urzua <chritianurzua124@gmail.com>
2024-07-10 14:48:30 -0400

    add javascript and button

--- 00-intro/intro.js
+++ 00-intro/intro.js
@@ -3,3 +3,17 @@
 //  1. Give every variable/const smallest scope
 //  2. Grab HTML elements by tag and store in const
 //  3. Put all other code inside functions
+
+const colorfield = document.getElementById("color-field")
+const fontsizefield = document.getElementById("font-size-field")
+const clickbutton = document.getElementById("click-button")
+const text = document.getElementById("text")
+const body=document.body
+//let
+//var
+function buttonpress() {
+    text.innerHTML="Wow! you pressed it";
+    body.style.backgroundColor=colorfield.value;
+    text.style.fontSize=`${fontsizefield.value}px`
+
+}

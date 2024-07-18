d11efaa8f461642904eac522ae7a73576bf52141
christian-urzua <chritianurzua124@gmail.com>
2024-07-18 15:12:52 -0400

    Last save

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const jumpscare = document.getElementById("jumpscare");
 function buttonPress(){
     if(jumpscare.style.visibility === "hidden") {
         jumpscare.style.visibility="visible"
+        jumpscare.style.position="sticky"
     } else {
         jumpscare.style.visibility="hidden"
     }
